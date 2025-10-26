41cd17d0f25a5ea9ceffe183ba4541175e0a6d87
Paulolima789 <maicon271271@gmail.com>
2025-10-26 16:54:39 -0400

    Remove arquivos e diretórios desnecessários do projeto Django Docker

--- django-docker-app/apps/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    # Adicione outras rotas específicas da aplicação core aqui
-]
--- django-docker-app/tests/test_basic.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-class BasicTest(TestCase):
-    def test_basic_functionality(self):
-        self.assertEqual(1 + 1, 2)  # Teste simples de adição
-        self.assertTrue(True)  # Teste de condição verdadeira
-        self.assertFalse(False)  # Teste de condição falsa
