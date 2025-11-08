6f7fcd7ed4de511db37ff7a3e58580adc8381fd7
Paulolima789 <maicon271271@gmail.com>
2025-11-08 17:46:23 -0400

    fix(group-detail): Replace GroupDeleteSerializer with GroupDetailSerializer for accurate group detail retrieval

--- BackEnd/api_django/api/views/group/detail.py
+++ BackEnd/api_django/api/views/group/detail.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import RetrieveAPIView
 from django.contrib.auth.models import Group
-from api.serializers.group import GroupDeleteSerializer
+from api.serializers.group import GroupDetailSerializer
 
 # autenticated
 from rest_framework.permissions import IsAuthenticated
@@ -12,13 +12,13 @@ from drf_yasg.utils import swagger_auto_schema
 class GroupDetailView(RetrieveAPIView):
     """Retorna os detalhes de um registro específico de Group."""
     queryset = Group.objects.all()
-    serializer_class = GroupDeleteSerializer
+    serializer_class = GroupDetailSerializer  # Corrigido: estava usando GroupDeleteSerializer
     permission_classes = [IsAuthenticated, IsAdmin | IsUser]
 
     @swagger_auto_schema(
         tags=["Groups"],
         operation_description="Recupera os detalhes de um registro de Group.",
-        responses={200: GroupDeleteSerializer()},
+        responses={200: GroupDetailSerializer()},
         operation_id="group_detail",
     )
     def get(self, request, *args, **kwargs):
