2368fbfb97f09088c2043655956ededb8d1d1b4f
Matheus Souza <m7hsouza@gmail.com>
2025-11-09 19:00:34 -0400

    feat: add formatDate utility function for date formatting in Portuguese

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,7 +1,15 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import type { ClassValue } from 'clsx';
 
 export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDate = (date: string | Date) => {
+  return format(new Date(date), "dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+};
