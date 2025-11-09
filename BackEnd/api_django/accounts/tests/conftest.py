c79658170ff39197380089d81b1ab55660ef3d26
Keven <kevennorman15@gmail.com>
2025-11-09 16:18:05 -0400

    test: adicionar testes para autenticação de usuários e login

---
+++ BackEnd/api_django/accounts/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(email="u@test.com", password="123456")
+
+
+@pytest.fixture
+def auth_client(user):
+    client = APIClient()
+    refresh = RefreshToken.for_user(user)
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token!s}")
+    return client
