ab27dd6333908df9c65cdb64d67ce9b2883fd6b3
Matheus Souza <m7hsouza@gmail.com>
2025-11-10 00:00:48 -0400

    feat: implement authentication context and integrate into application structure

---
+++ web/src/auth.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import api from './lib/axios';
+
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+interface AuthenticatedUser {
+  id: string;
+  email: string;
+  roles: Array<string>;
+}
+
+export interface AuthContext {
+  isAuthenticated: boolean;
+  login: (credentials: Credentials) => Promise<void>;
+  logout: () => Promise<void>;
+  user: AuthenticatedUser | null;
+}
+
+const AuthContext = React.createContext<AuthContext | null>(null);
+
+const key = 'tanstack.auth.user';
+
+function getStoredUser() {
+  const user = localStorage.getItem(key);
+  return user ? JSON.parse(user) : null;
+}
+
+function setStoredUser(user: AuthenticatedUser | null) {
+  if (user) {
+    localStorage.setItem(key, JSON.stringify(user));
+  } else {
+    localStorage.removeItem(key);
+  }
+}
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = React.useState<AuthenticatedUser | null>(
+    getStoredUser(),
+  );
+  const isAuthenticated = !!user;
+
+  const logout = React.useCallback(async () => {
+    await api.post(
+      '/auth/sign-out',
+      {},
+      {
+        withCredentials: true,
+      },
+    );
+    setStoredUser(null);
+    setUser(null);
+  }, []);
+
+  const login = React.useCallback(async (credentials: Credentials) => {
+    const { data } = await api.post(
+      '/auth/sign-in/email',
+      {
+        ...credentials,
+        callbackURL: window.location.origin + '/admin',
+      },
+      {
+        withCredentials: true,
+      },
+    );
+
+    setStoredUser(data.user);
+    setUser(user);
+  }, []);
+
+  React.useEffect(() => {
+    setUser(getStoredUser());
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function getContext() {
+  const context = React.useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
--- web/src/pages/__root.tsx
+++ web/src/pages/__root.tsx
@@ -5,10 +5,12 @@ import { TanStackDevtools } from '@tanstack/react-devtools';
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 
 import type { QueryClient } from '@tanstack/react-query';
+import type { AuthContext } from '@/auth';
 import { Toaster } from '@/components/ui/sonner';
 
 interface MyRouterContext {
   queryClient: QueryClient;
+  auth: AuthContext;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
